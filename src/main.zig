176990d5b97f61b330dfd4693f218cd0753a6b94
fn3x <fn3x@proton.me>
2024-08-02 07:08:51 +0400

    feat: we are drawing something

--- src/main.zig
+++ src/main.zig
@@ -9,43 +9,74 @@ pub fn main() !void {
     defer arena.deinit();
     const allocator = arena.allocator();
 
-    var state = try State.init(allocator, 5, 3);
+    const size = 3;
+    const mines = 1;
+
+    var state = try State.init(allocator, size, mines);
     defer state.deinit(allocator);
 
-    const window = SDL2.SDL_CreateWindow("Simple", 0, 0, 200, 200, 0);
+    const window = SDL2.SDL_CreateWindow("Simple", 0, 0, 400, 400, 0);
     defer SDL2.SDL_DestroyWindow(window);
 
-    const clicked = 23;
+    const clicked = 4;
 
-    const visit = state.visitField(1);
-    std.log.info("Visit at {d}: {any}", .{ clicked, visit });
-    state.print();
+    _ = state.visitField(clicked);
 
     const renderer = SDL2.SDL_CreateRenderer(window, 0, SDL2.SDL_RENDERER_PRESENTVSYNC);
     defer SDL2.SDL_DestroyRenderer(renderer);
 
-    mainloop: while (true) {
-        var sdl_event: SDL2.SDL_Event = undefined;
+    // const surface = SDL2.SDL_GetWindowSurface(window);
 
+    var sdl_event: SDL2.SDL_Event = undefined;
+    var rects = try allocator.alloc(SDL2.SDL_Rect, size * size);
+    defer allocator.free(rects);
+
+    const shift_y = 25;
+    const shift_x = 25;
+    const field_size = 20;
+
+    inline for (0..size * size) |i| {
+        rects[i].h = field_size;
+        rects[i].w = field_size;
+
+        if (i == 0) {
+            rects[i].x = shift_x;
+            rects[i].y = shift_y;
+        } else if (i % size == 0) {
+            rects[i].x = shift_x;
+            rects[i].y = rects[i - 1].y + shift_y;
+        } else {
+            rects[i].x = rects[i - 1].x + shift_x;
+            rects[i].y = rects[i - 1].y;
+        }
+    }
+
+    for (0..size * size) |i| {
+        std.log.info("{any}\n", .{rects[i]});
+    }
+
+    main_loop: while (true) {
         while (SDL2.SDL_PollEvent(&sdl_event) != 0) {
             switch (sdl_event.type) {
-                SDL2.SDL_QUIT => break :mainloop,
+                SDL2.SDL_QUIT => break :main_loop,
                 SDL2.SDL_KEYDOWN => {
                     if (sdl_event.key.keysym.sym == SDL2.SDLK_ESCAPE) {
-                        break :mainloop;
+                        break :main_loop;
                     }
                 },
                 else => {},
             }
         }
 
-        _ = SDL2.SDL_RenderClear(renderer);
+        _ = SDL2.SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
-        var rect = SDL2.SDL_Rect{ .x = 0, .y = 0, .w = 60, .h = 60 };
+        _ = SDL2.SDL_RenderClear(renderer);
 
         _ = SDL2.SDL_SetRenderDrawColor(renderer, 0xff, 0, 0, 0xff);
 
-        _ = SDL2.SDL_RenderFillRect(renderer, &rect);
+        for (rects) |rect| {
+            _ = SDL2.SDL_RenderFillRect(renderer, &rect);
+        }
 
         SDL2.SDL_RenderPresent(renderer);
     }
